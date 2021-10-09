45e467b990385899fbda4b584fa82a652b03fdcb
Nikolay Zamotaev <fhunter@fhunter.mine.nu>
2021-10-09 17:35:43 +0300

    Updated password generation script for user creation

--- scripts/create_principal.sh
+++ scripts/create_principal.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+PASS=$(pwgen -c -n -B -1 8)
 kadmin -p automator/admin -k -t /etc/krb5.keytab << EOF
-addprinc -policy "$1" -pw "__$2" "$2"
+addprinc -policy "$1" -pw "${PASS}" "$2"
 modprinc +needchange "$2"
 EOF
+echo "$2" "$PASS" >> "$3"
