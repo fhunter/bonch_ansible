0b87e8ac59f3ee20c24a76ef3e2d67b5ca507d71
Nikolay Zamotaev <fhunter@fhunter.mine.nu>
2021-11-30 12:16:13 +0300

    Script updates

--- scripts/create_principal.sh
+++ scripts/create_principal.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-LOCALDIR=$(dirname "$(readlink -f "$0")")
-PASS=$(${LOCALDIR}/gpw.py)
-#PASS=$(pwgen -A -0 -B -1 6)
+PASS=$(pwgen -A -0 -B -1 6)
 kadmin -p automator/admin -k -t /etc/krb5.keytab << EOF
 addprinc -policy "$1" -pw "${PASS}" "$2"
 modprinc +needchange "$2"
