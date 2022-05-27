d4346edf03f0aad23439ce1dcd3282be1c42ac91
Nikolay Zamotaev <fhunter@fhunter.mine.nu>
2022-05-27 17:23:31 +0300

    Fixed mistakes in code

--- scripts/triggerreset.py
+++ scripts/triggerreset.py
@@ -7,3 +7,4 @@ import sys
 if len(sys.argv) > 1:
 	data = { 'username': sys.argv[1] }
 	response = requests.post('http://eniac.dcti.sut.ru/selfreg/process/newuser',auth=HTTPKerberosAuth(), json = data)
+	print(response.json())
