7ccc077fb90a56af1aa60dc3d6c6ecbb82f9240b
Nikolay Zamotaev <fhunter@fhunter.mine.nu>
2023-02-08 19:51:01 +0300

    Fixed scripts for creation/deletion users - now they use numbered 'automator[0-9]' principals

--- scripts/delete_principal.sh
+++ scripts/delete_principal.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-kadmin -p automator/admin -k -t /etc/krb5.keytab << EOF
+HOST=$(hostname|awk -F. '{print $1}')
+PRINCIPAL="automator"
+case "$HOST" in
+    "srv-1") PRINCIPAL="automator1" ;;
+    "srv-2") PRINCIPAL="automator2" ;;
+esac
+kadmin -p ${PRINCIPAL}/admin -k -t /etc/krb5.keytab << EOF
 delprinc "$2"
 EOF
