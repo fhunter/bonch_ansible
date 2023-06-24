671f23efe3cb802b7b955042ca4bcbf17d00e230
Nikolay Zamotaev <fhunter@fhunter.mine.nu>
2023-06-24 05:01:38 +0300

    Fixed triggerreset script

--- scripts/triggerreset.py
+++ scripts/triggerreset.py
@@ -1,10 +1,12 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import requests
-from requests_kerberos import HTTPKerberosAuth
+from requests_kerberos import HTTPKerberosAuth, OPTIONAL, DISABLED, REQUIRED
 import sys
 
 if len(sys.argv) > 1:
 	data = { 'username': sys.argv[1] }
-	response = requests.post('http://eniac.dcti.sut.ru/selfreg/process/newuser',auth=HTTPKerberosAuth(), json = data)
+	kerberos_auth = HTTPKerberosAuth(mutual_authentication=REQUIRED)
+	response = requests.post('http://srv-1.dcti.sut.ru/selfreg/process/newuser',auth=kerberos_auth, json = data)
+	print(response.text)
 	print(response.json())
