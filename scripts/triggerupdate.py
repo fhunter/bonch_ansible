1184b54b49b375c5eb00d8bcd80312063fb91a07
Nikolay Zamotaev <fhunter@fhunter.mine.nu>
2023-06-24 05:40:41 +0300

    Fixed server error on user creation

---
+++ scripts/triggerupdate.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import requests
+from requests_kerberos import HTTPKerberosAuth, OPTIONAL, DISABLED, REQUIRED
+import sys
+
+kerberos_auth = HTTPKerberosAuth(mutual_authentication=REQUIRED)
+response = requests.get('http://srv-1.dcti.sut.ru/selfreg/groups',auth=kerberos_auth)
+print(response)
