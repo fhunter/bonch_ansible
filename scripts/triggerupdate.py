08672c13fa57285bf0f2d58063daf8fd07206917
fhunter <fhunter@fhunter.link>
2023-06-24 15:47:05 +0200

    update url

--- scripts/triggerupdate.py
+++ scripts/triggerupdate.py
@@ -5,5 +5,5 @@ from requests_kerberos import HTTPKerberosAuth, OPTIONAL, DISABLED, REQUIRED
 import sys
 
 kerberos_auth = HTTPKerberosAuth(mutual_authentication=REQUIRED)
-response = requests.get('http://srv-1.dcti.sut.ru/selfreg/groups',auth=kerberos_auth)
+response = requests.get('http://srv-1.dcti.sut.ru/selfreg/resync',auth=kerberos_auth)
 print(response)
