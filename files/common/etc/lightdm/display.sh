379274798a3c3a89d792881de46fb4906c7b3ae6
fhunter <fhunter@fhunter.link>
2023-08-19 17:51:44 +0200

    Fixed login screen background script allowing for different filenames

--- files/common/etc/lightdm/display.sh
+++ files/common/etc/lightdm/display.sh
@@ -9,8 +9,9 @@ rm /tmp/background.jpg
 while [ \! -d /afs/dcti.sut.ru/media/wallpapers/xdm ];do
 	sleep 1
 done
-count=`ls -1 /afs/dcti.sut.ru/media/wallpapers/xdm | wc -l`
-rand=1`date +%N`
-num=$((rand%count))
-convert /afs/dcti.sut.ru/media/wallpapers/xdm/${num}.jpg -quality 100 /tmp/background.jpg
+count=$(ls -1 /afs/dcti.sut.ru/media/wallpapers/xdm | wc -l)
+rand=1$(date +%N)
+num=$((rand%count + 1))
+filename="$(ls -1 /afs/dcti.sut.ru/media/wallpapers/xdm|head -n ${num}|tail -n 1)"
+convert "/afs/dcti.sut.ru/media/wallpapers/xdm/${filename}" -quality 100 /tmp/background.jpg
 
